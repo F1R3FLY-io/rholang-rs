f0c517eef6b8ecad8d11261f5674c3feb1187856
MarcinR-DevBrother <marcin.r@devbrother.com>
2025-09-10 21:23:06 +0200

    Initial implementation sketch of the semantic db

--- rholang-lib/src/sem/mod.rs
+++ rholang-lib/src/sem/mod.rs
@@ -2,13 +2,12 @@ use by_address::ByAddress;
 use indexmap::IndexMap;
 use rholang_parser::ast;
 
-mod alg;
 mod db;
 
 pub type ProcRef<'a> = &'a ast::AnnProc<'a>;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub struct PID(i32);
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PID(u32);
 
 pub struct SemanticDb<'a> {
     rev: IndexMap<ByAddress<ProcRef<'a>>, PID>,
